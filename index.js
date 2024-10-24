efb2e40970f5d6b48ae70189570fbc9600180aa6
MarcioAlex-x <marcioalex586@gmail.com>
2024-10-24 09:26:28 -0300

    24/10

--- index.js
+++ index.js
@@ -23,6 +23,41 @@ const conn = mysql.createConnection({
 app.get('/',(req,res)=>{
     res.render('home')
 })
+app.get('/amigo/:id',(req,res)=>{
+    const id = req.params.id
+    const sql = `SELECT * FROM amigos WHERE id = ${id}`
+    conn.query(sql, id,(err, data)=>{
+        if(err){
+            console.log(err)
+            return
+        }
+        const amigo = data[0]
+        res.render('amigo',{amigo})
+    })
+})
+
+app.get('/amigos',(req,res)=>{
+    const sql = 'SELECT * FROM amigos'
+    conn.query(sql,(err, data)=>{
+        if(err){
+            return
+        }
+        const amigos = data
+        res.render('amigos',{amigos})
+    })
+})
+
+app.post('/apagar/:id',(req,res)=>{
+    const id = req.params.id
+    const sql = `DELETE FROM amigos WHERE id = ${id}`
+    conn.query(sql,(err)=>{
+        if(err){
+            console.log(err)
+            return
+        }
+        res.redirect('/amigos')
+    }) 
+})
 
 app.post('/adicionar',(req,res)=>{
     const nome = req.body.nome
@@ -35,6 +70,35 @@ app.post('/adicionar',(req,res)=>{
         return
     })
     res.redirect('/')
+})
+
+app.get('/formEditar/:id',(req,res)=>{
+    const id = req.params.id
+    const sql = `SELECT * FROM amigos WHERE id = ${id}`
+    conn.query(sql,(err, data)=>{
+        if(err){
+            console.log(err)
+            return
+        }
+        const amigo = data[0]
+        console.log(data)
+        res.render('editar',{amigo})
+    })    
+})
+
+app.post('/editar',(req,res)=>{
+    const id = req.body.id
+    const nome = req.body.nome
+    const idade = req.body.idade
+
+    const sql = `UPDATE amigos SET nome='${nome}', idade='${idade}' WHERE id = ${id}`
+    conn.query(sql,(err)=>{
+        if(err){
+            console.log(err)
+            return
+        }
+        res.redirect('/')
+    })
 })
 
 
